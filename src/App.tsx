188fafa909761f6fa64da51963e387d5d0e46de1
Sabin <sabinonweb7@gmail.com>
2024-05-01 00:55:14 +0545

    [Chore: Prelim LoginPage]

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import "./index.css";
+import LoginPage from "./components/LoginPage";
 
-import NavBar from "./components/NavBar";
-
-function App() {
-  let items = ["Home", "Videochat", "Text", "profile"];
-
-  return (
-    <div>
-      <NavBar brandName="STUDYBUDDY" navItems={items} />
-      <div></div>
-    </div>
-  );
-}
+const App = () => {
+  return <LoginPage />;
+};
 
 export default App;
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,40 +1,28 @@
-import React from "react";
+import Logo from "../assets/logo.png";
+import Login from "../assets/login.svg?react";
 
-function LoginPage() {
+const LoginPage = () => {
   return (
-    <>
-      <div className="container">
-        <h1>Welcome Back!</h1>
-        <form>
-          <input
-            className="formm"
-            type="text"
-            placeholder="Username"
-            required
-          ></input>
-          <input
-            className="formm"
-            type="Password"
-            placeholder="Password"
-            required
-          ></input>
-
-          <input id="myinput" type="checkbox"></input>
-          <label htmlFor="myinput">Remember Me</label>
-          <a className="a1" href="#">
-            Forgot Password?
-          </a>
-        </form>
-        <button className="btn">Login</button>
-        <p>
-          Don't have an account?
-          <a className="a2" href="Register.tsx">
-            Click here
-          </a>
-        </p>
+    <div className="flex flex-row w-screen h-screen">
+      <div className="flex flex-col w-1/2 bg-red-500 space-y-16">
+        <div className="flex flex-row ml-10 mt-4">
+          <img src={Logo} className="size-14" />
+          <h1 className="text-3xl font-semibold ml-2 mt-2">studybuddy</h1>
+        </div>
+        <div className="flex justify-center">
+          <div className="flex flex-col justify-between items-center border-2 border-emerald-950 w-11/12 rounded-lg h-9/12">
+            <h1 className="text-emerald-950 font-bold">Login</h1>
+            <div>
+              <input />
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+      <div className="flex w-1/2">
+        <Login className="h-screen size-10/12" />
+      </div>
+    </div>
   );
-}
+};
 
 export default LoginPage;
