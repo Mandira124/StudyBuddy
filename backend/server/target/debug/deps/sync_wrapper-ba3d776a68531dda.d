3f2194d53afe9224a0f2798dac3681220aa6b9ab
Sabin <123313687+sabinonweb@users.noreply.github.com>
2024-05-23 14:19:43 +0545

    Delete backend/server/target directory

--- backend/server/target/debug/deps/sync_wrapper-ba3d776a68531dda.d
+++
@@ -1,7 +0,0 @@
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/deps/libsync_wrapper-ba3d776a68531dda.rmeta: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/sync_wrapper-1.0.1/src/lib.rs
-
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/deps/libsync_wrapper-ba3d776a68531dda.rlib: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/sync_wrapper-1.0.1/src/lib.rs
-
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/deps/sync_wrapper-ba3d776a68531dda.d: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/sync_wrapper-1.0.1/src/lib.rs
-
-/Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/sync_wrapper-1.0.1/src/lib.rs:
