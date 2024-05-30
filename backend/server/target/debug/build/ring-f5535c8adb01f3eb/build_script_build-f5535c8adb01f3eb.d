3705653c5c9b277774c62512439c357799996ba9
Sabin <123313687+sabinonweb@users.noreply.github.com>
2024-05-30 23:12:09 +0545

    Delete backend/server/target directory

--- backend/server/target/debug/build/ring-f5535c8adb01f3eb/build_script_build-f5535c8adb01f3eb.d
+++
@@ -1,5 +0,0 @@
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/build/ring-f5535c8adb01f3eb/build_script_build-f5535c8adb01f3eb: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ring-0.17.8/build.rs
-
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/build/ring-f5535c8adb01f3eb/build_script_build-f5535c8adb01f3eb.d: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ring-0.17.8/build.rs
-
-/Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ring-0.17.8/build.rs:
--- backend/server/target/debug/deps/byteorder-dd99fb2d3d92ed50.d
+++
@@ -1,7 +0,0 @@
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/deps/libbyteorder-dd99fb2d3d92ed50.rmeta: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/deps/libbyteorder-dd99fb2d3d92ed50.rlib: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/Users/sabinonweb/Documents/Projects/StudyBuddy/backend/server/target/debug/deps/byteorder-dd99fb2d3d92ed50.d: /Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs
-
-/Users/sabinonweb/.cargo/registry/src/index.crates.io-6f17d22bba15001f/byteorder-1.5.0/src/lib.rs:
