9612d615279995f768cb539e9d929ee62f6055ab
Mandira Sharma <smandira984@gmail.com>
2024-06-02 17:42:23 +0545

    home1 and home2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,11 @@ import LandingPage from "./components/LandingPage";
 import NavBar from "./components/NavBar";
 import RegisterPage from "./components/auth/RegisterPage";
 import LoginPage from "./components/auth/LoginPage";
+import Home1 from "./components/Home1";
+import Home2 from "./components/Home2";
 
 const App = () => {
-  return <LoginPage />;
+  return <Home2 />;
 };
 
 export default App;
