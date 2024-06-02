1a74fc25ab4d4fc670c33dc26e098de2bceac3eb
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-02 17:45:10 +0545

    Your descriptive commit message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import LandingPage from "./components/LandingPage";
 import NavBar from "./components/NavBar";
 import RegisterPage from "./components/auth/RegisterPage";
+import Profile from "./components/profile" 
 import LoginPage from "./components/auth/LoginPage";
 
 const App = () => {
-  return <LoginPage />;
+  return <Profile />;
 };
 
 export default App;
