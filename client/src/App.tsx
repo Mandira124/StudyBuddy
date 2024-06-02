ae020c6f67bac36e2707977e3a36eacaddc84e91
Mandira Sharma <smandira984@gmail.com>
2024-06-02 23:50:55 +0545

    navigation of homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,28 @@
-import LandingPage from "./components/LandingPage";
-import NavBar from "./components/NavBar";
-import RegisterPage from "./components/auth/RegisterPage";
-import Profile from "./components/profile"
-import LoginPage from "./components/auth/LoginPage";
-import logo from "./assets/logo.png"
-const App = () => {
-  return <Home2 />;
-};
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home1 from "./components/Home1";
+import Home2 from "./components/Home2";
+import Home3 from "./components/Home3";
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+import Home4 from "./components/Home4";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home1 />} />
+          <Route path= "home2" element={<Home2 />} />
+          <Route path="home3" element={<Home3 />} />
+          <Route path="home4" element={<Home4/>}/>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
 
 export default App;
- 
