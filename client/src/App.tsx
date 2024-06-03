2fd97ad3aa20dbd483500bc53b76bf51a52f8586
Mandira Sharma <smandira984@gmail.com>
2024-06-04 00:04:25 +0545

    Routing added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,27 @@
-import LandingPage from "./components/LandingPage";
-import NavBar from "./components/NavBar";
+import NavBar from "./NavBar/NavBar";
 import RegisterPage from "./components/auth/RegisterPage";
-import Profile from "./components/profile"
+import Profile from "./components/profile";
 import LoginPage from "./components/auth/LoginPage";
-import logo from "./assets/logo.png"
-import CommunityPosts from "./components/communityPost";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import logo from "./assets/logo.png";
+import { BrowserRouter as Router,useNavigate,Route,Routes } from "react-router-dom";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Home1 from "./components/Home1";
+
 
 const App = () => {
-  let items = ["Home", "Text", "Videochat", "Profile"]
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./Layout";
-import Home1 from "./components/Home1";
-import Home2 from "./components/Home2";
-import Home3 from "./components/Home3";
-import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
-import Home4 from "./components/Home4";
-
-function App() {
+  
+  let items = ["Home", "Text", "Videochat", "Profile"];
   return (
-  <div>
-  <NavBar brandName="StudyBuddy" imageSrcPath={logo}
-    navItems={items} />
-    </div>
+    <Router>
+      <div>
+        <NavBar brandName="StudyBuddy" imageSrcPath={logo} navItems={items} />
+        <Routes>
+          <Route path="/" element={<Home1/>} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
--- client/src/components/Home4.tsx
+++ client/src/components/Home4.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import home from "../assets/home4.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Home4 = () => {
+  const navigate= useNavigate();
+
+  const goToLogin=()=>{
+    navigate("/loginpage");
+  }
   return (
     <div className="flex flex-1 w-11/12 items-center justify-between space-x-10 ml-40">
         <img src={home} alt="Home" className="size-3/5 " />
@@ -10,7 +16,7 @@ const Home4 = () => {
         <p className="text-5xl w-3/4 ">Connect globally, discuss topics, and achieve academic success.</p>
         <div className="h-20">
         </div>
-        <button className="bg-green-900 text-white py-4 px-4 w-2/4  font-bold rounded-2xl text-4xl">
+        <button className="bg-green-900 text-white py-4 px-4 w-2/4  font-bold rounded-2xl text-4xl" onClick={goToLogin}>
             Dive In
         </button>
         </div>  
