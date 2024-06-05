a771dea673c8389030e4fd4c1b61812f3928aa3e
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-05 17:33:20 +0545

    server cp

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
 import NavBar from "./NavBar/NavBar";
 import logo from "./assets/logo.png";
-
+import CommunityPosts from "./components/communityPost";
 const Layout = () => {
-  let items = ["Home", "Text", "Videochat", "Profile"];
-  return (
-    <div className="flex flex-col min-h-screen">
-      <NavBar brandName="StudyBuddy" imageSrcPath={logo} navItems={items} />
-      <main className="flex-grow">
-      <Outlet />
-      </main>
-    </div>
-  );
+  // let items = ["Home", "Text", "Videochat", "Profile"];
+  // return (
+  //   <div className="flex flex-col min-h-screen">
+  //     <NavBar brandName="StudyBuddy" imageSrcPath={logo} navItems={items} />
+  //     <main className="flex-grow">
+  //     <Outlet />
+  //     </main>
+  //   </div>
+  // );
+  return <CommunityPosts/>
 };
 
 export default Layout;
