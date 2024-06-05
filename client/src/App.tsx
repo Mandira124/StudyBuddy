26baf79931c962438689605edff4b7dc83787cf7
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-05 21:04:26 +0545

    cp update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,16 +5,19 @@ import Profile from "./pages/profile";
 import LoginPage from "./pages/auth/LoginPage";
 import logo from "./assets/logo.png"
 import { Outlet } from "react-router-dom";
+import CommunityPosts from "./pages/CommunityPost";
 
 const App = () => {
   let items = ["Home", "Text", "Videochat", "Profile"]
   return (
     <div className="flex flex-col min-h-screen">
+      <NavBar brandName="StudyBuddy" imageSrcPath={logo} navItems={items} />
       <main className="flex-grow">
         <Outlet />
       </main>
     </div>
   )
+  // return <CommunityPosts/>
 }
 
 export default App;
