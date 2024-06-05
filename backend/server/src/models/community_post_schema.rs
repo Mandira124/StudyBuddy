347fbc6fbe6a044fed1ea7d22aba8ce22906b046
Sabin <sabinonweb7@gmail.com>
2024-06-06 01:06:50 +0545

    [Update: CommunityPost]

---
+++ backend/server/src/models/community_post_schema.rs
@@ -0,0 +1,22 @@
+use mongodb::bson::oid::ObjectId;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CommunityPostSchema {
+    pub _id: Option<ObjectId>,
+    pub username: String,
+    pub post_id: String, 
+    pub upvotes: u32,
+    pub downvotes: u32,
+    pub subject: String,
+    pub post_content: String,
+    pub profile_pic: Option<String>,
+    pub comment: Vec<String>,   
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CommunityPostsSchema {
+    pub posts: Vec<CommunityPostSchema>,
+    pub most_liked: CommunityPostSchema,
+    pub hot_posts: Vec<CommunityPostSchema>,    
+}
