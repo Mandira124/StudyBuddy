449fc74c260373f5a2b8c01f8dc832c767830cbb
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-05 21:04:26 +0545

    cp update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import Router from "./Router";
 import "./styles/index.css";
 import "./styles/App.css";
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router></Router>
