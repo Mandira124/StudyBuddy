7dc22b9b4314c968b7ceb06655a645cceaf73224
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-05 17:33:20 +0545

    server cp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,49 +1,20 @@
-<<<<<<< HEAD
-// import RegisterPage from "./components/RegisterPage";
-// import LoginPage from "./components/auth/LoginPage";
-// import AuthProvider from "./context/AuthContext";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-//
-// const App = () => {
-//   return (
-//     <>
-//       <Router>
-//         <AuthProvider>
-//           <Routes>
-//             <Route path="/login" element={<LoginPage />} />
-//             <Route path="/register" element={<RegisterPage />} />
-//           </Routes>
-//         </AuthProvider>
-//       </Router>
-//     </>
-//   );
-// };
-//
-// export default App;
-
-import LoginPage from "./components/auth/LoginPage";
-import RegisterPage from "./components/auth/RegisterPage";
-
-export default function App() {
-  return <RegisterPage />;
-}
-=======
 import LandingPage from "./components/LandingPage";
 import NavBar from "./NavBar/NavBar";
 import RegisterPage from "./components/auth/RegisterPage";
 import Profile from "./components/profile"
 import LoginPage from "./components/auth/LoginPage";
 import logo from "./assets/logo.png"
+import CommunityPosts from "./components/communityPost";
 
 const App = () => {
-  let items = ["Home", "Text", "Videochat", "Profile"]
-  return (
-    <div>
-      <NavBar brandName="StudyBuddy" imageSrcPath={logo}
-    navItems={items} />
-    </div>
-  )
+  // let items = ["Home", "Text", "Videochat", "Profile"]
+  // return (
+  //   <div>
+  //     <NavBar brandName="StudyBuddy" imageSrcPath={logo}
+  //   navItems={items} />
+  //   </div>
+  // )
+  return <CommunityPosts/>
 }
 
 export default App;
->>>>>>> df3eba1f796ef37ebfd8cdce24db6f02ad839b29
