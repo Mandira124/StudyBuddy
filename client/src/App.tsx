bb11c7a45e3e8dc8c60259bcdaa0d35317a87fe5
beacon404 <paharisanjay10@gmail.com>
2024-06-06 21:16:55 +0000

    videochat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,27 @@ import RegisterPage from "./components/auth/RegisterPage";
 import Profile from "./components/profile"
 import LoginPage from "./components/auth/LoginPage";
 import logo from "./assets/logo.png"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LobbyScreen from "./pages/lobby/Lobby";
+import RoomPage from "./pages/room/Room";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LobbyScreen />,},
+    {
+      path: "/room/:roomId",
+        element: <RoomPage />,
+    }
+]);
+
 const App = () => {
   let items = ["Home", "Text", "Videochat", "Profile"]
   return (
-  <Profile />
+    <RouterProvider router={router} />
   );
 };
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "../src/styles/index.css";
+import VideoChat from "./pages/lobby/Lobby.tsx";
+import { SocketProvider } from "./context/SocketProvider.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <SocketProvider>
+  <App />
+</SocketProvider>
+  // </React.StrictMode>
 );
