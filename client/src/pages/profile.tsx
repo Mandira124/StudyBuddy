79c40b1ec645fb23af6d8a2050d6a5d006ade04b
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-06 10:33:38 +0545

    navbar update

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import profilePic from "../assets/profile.png";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import Sidebar from "./SideBar";
 
 const Profile = () => {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -16,65 +17,7 @@ const Profile = () => {
 
   return (
     <div className="flex flex-col lg:flex-row min-h-screen">
-      <div className="lg:w-1/6 bg-green-800 p-4 h-screen rounded-xl overflow-y-auto">
-        <div className="text-white font-bold mb-4 text-xl">Topics</div>
-        <div className="flex flex-col space-y-2">
-          <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-            <i className="fas fa-fire text-lg"></i>
-            <span>Trending</span>
-          </button>
-          <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-            <i className="fas fa-fire-alt text-lg"></i>
-            <span>Hot</span>
-          </button>
-          <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-            <i className="fas fa-thumbs-up text-lg"></i>
-            <span>Liked</span>
-          </button>
-          <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm" onClick={toggleDropdown}>
-            <i className="fas fa-caret-down text-lg"></i>
-            <span>Subject</span>
-          </button>
-          {showDropdown && (
-            <>
-              <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-                <i className="fas fa-book text-lg"></i>
-                <span>Button 4</span>
-              </button>
-              <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-                <i className="fas fa-book text-lg"></i>
-                <span>Button 5</span>
-              </button>
-              <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-                <i className="fas fa-book text-lg"></i>
-                <span>Button 6</span>
-              </button>
-              <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-                <i className="fas fa-book text-lg"></i>
-                <span>Button 7</span>
-              </button>
-              <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-                <i className="fas fa-book text-lg"></i>
-                <span>Button 8</span>
-              </button>
-              <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-                <i className="fas fa-book text-lg"></i>
-                <span>Button 9</span>
-              </button>
-              <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-sm">
-                <i className="fas fa-book text-lg"></i>
-                <span>Button 10</span>
-              </button>
-            </>
-          )}
-        </div>
-        <div>
-          <button className="mt-auto p-2 text-green-800 bg-white hover:bg-white hover:text-green-800 transition-transform transform hover:scale-110 rounded-full text-sm" onClick={handleCreatePostClick}>
-            <i className="fas fa-plus"></i>
-            <span>Create Post</span>
-          </button>
-        </div>
-      </div>
+      <Sidebar onCreatePostClick={handleCreatePostClick} />
 
       <div className="flex flex-col lg:w-5/6">
         <div className="relative p-4">
