f1057fdf33a626d3eae14d266190c9df423c0178
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-06 10:33:38 +0545

    navbar update

--- client/src/pages/SideBar.tsx
+++ client/src/pages/SideBar.tsx
@@ -13,46 +13,38 @@ const Sidebar: React.FC<SidebarProps> = ({ onCreatePostClick }) => {
 
   return (
     <div className="lg:w-1/6 bg-green-800 p-4 h-screen rounded-xl overflow-y-auto">
-      <div className="text-white font-bold mb-4 text-xl sm:text-xl md:text-xl lg:text-xl">
+      <div className="text-white font-bold mb-4 text-base">
         Topics
       </div>
       <div className="flex flex-col space-y-2">
-        <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-xl sm:text-xl md:text-2xl lg:text-3xl">
-          <i className="fas fa-fire sm:text-sm md:text-base lg:text-xl"></i>
-          <span className="text-sm sm:text-sm md:text-sm lg:text-lg">
-            Trending
-          </span>
+        <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-base">
+          <i className="fas fa-fire text-base"></i>
+          <span>Trending</span>
         </button>
-        <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-xl sm:text-xl md:text-2xl lg:text-3xl">
-          <i className="fas fa-fire-alt sm:text-sm md:text-base lg:text-xl"></i>
-          <span className="text-sm sm:text-sm md:text-sm lg:text-lg">Hot</span>
+        <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-base">
+          <i className="fas fa-fire-alt text-base"></i>
+          <span>Hot</span>
         </button>
-        <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-xl sm:text-xl md:text-2xl lg:text-3xl">
-          <i className="fas fa-thumbs-up sm:text-sm md:text-base lg:text-xl"></i>
-          <span className="text-sm sm:text-sm md:text-sm lg:text-lg">
-            Liked
-          </span>
+        <button className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-base">
+          <i className="fas fa-thumbs-up text-base"></i>
+          <span>Liked</span>
         </button>
         <button
-          className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-xl sm:text-xl md:text-2xl lg:text-3xl"
+          className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-base"
           onClick={toggleDropdown}
         >
-          <i className="fas fa-caret-down sm:text-sm md:text-base lg:text-xl"></i>
-          <span className="text-sm sm:text-sm md:text-sm lg:text-lg">
-            Subject
-          </span>
+          <i className="fas fa-caret-down text-base"></i>
+          <span>Subject</span>
         </button>
         {showDropdown && (
           <>
             {Array.from({ length: 7 }, (_, i) => (
               <button
                 key={i}
-                className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105"
+                className="p-2 text-white flex items-center space-x-2 rounded transition-transform transform hover:scale-105 text-base"
               >
-                <i className="fas fa-book"></i>
-                <span className="text-base sm:text-base md:text-xl lg:text-2xl">
-                  Button {i + 4}
-                </span>
+                <i className="fas fa-book text-base"></i>
+                <span>Button {i + 4}</span>
               </button>
             ))}
           </>
@@ -60,13 +52,11 @@ const Sidebar: React.FC<SidebarProps> = ({ onCreatePostClick }) => {
       </div>
       <div>
         <button
-          className="mt-auto p-2 text-green-800 bg-white hover:bg-white hover:text-green-800 transition-transform transform hover:scale-110 rounded-full"
+          className="mt-auto p-2 text-green-800 bg-white hover:bg-white hover:text-green-800 transition-transform transform hover:scale-110 rounded-full text-base"
           onClick={onCreatePostClick}
         >
-          <i className="fas fa-plus"></i>
-          <span className="text-base sm:text-sm md:text-base lg:text-">
-            Create Post
-          </span>
+          <i className="fas fa-plus text-base"></i>
+          <span>Create Post</span>
         </button>
       </div>
     </div>
