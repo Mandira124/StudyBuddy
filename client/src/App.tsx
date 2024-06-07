a50a67ea116b9348f4c2bf4166eccf806eadd1cb
dikshya lamichhane <lamichhanedikshya7@gmail.com>
2024-06-07 18:49:31 +0545

    UPDATE:FRONTEND

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
 import { Outlet } from "react-router-dom";
+import CommentPage from "./pages/comment";
 
 const App = () => {
+  const postId=1
   return <Outlet />;
+  // return <CommentPage postId={postId}/>;
 };
 
 export default App;
