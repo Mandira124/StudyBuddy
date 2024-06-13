e064548ec37c07f12153f59f64f7e2ac1cc57680
Sabin <123313687+sabinonweb@users.noreply.github.com>
2024-06-14 01:25:32 +0545

    [Update: JWT]

--- VideoGo/go.mod
+++
@@ -1,9 +0,0 @@
-module VideoGo
-
-go 1.22.3
-
-require (
-	github.com/gorilla/websocket v1.5.1 // indirect
-	github.com/rs/cors v1.11.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-)
--- backend/target/debug/deps/backend.d
+++
@@ -1,5 +0,0 @@
-C:\Users\LENOVO\Desktop\StudyBuddy\backend\target\debug\deps\backend.exe: src\main.rs
-
-C:\Users\LENOVO\Desktop\StudyBuddy\backend\target\debug\deps\backend.d: src\main.rs
-
-src\main.rs:
